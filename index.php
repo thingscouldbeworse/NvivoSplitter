7e632ca38ea384e2fb1a5d365bc52bc82cac23fc
thingscouldbeworse <megal33tdude@gmail.com>
2016-07-12 16:23:28 -0400

    done

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<head>
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+	<script src="jquery-csv.js"></script>
+	<script type="text/javascript" src="FileSaver.js"></script>
+	<script type="text/javascript" src="jszip/dist/jszip.js"></script>
+	<script type="text/javascript" src="split.js"></script>
+	<link rel="stylesheet" href="style.css" type="text/css" media="all">
+</head>
+
+<div class="box" >
+	<label class="uploadBTN">
+		<input type="file" id="file" name="file"  enctype="multipart/form-data" />
+		<span> Choose File </span>
+	</label>
+</div>
+<script>
+	
+    document.getElementById('file').addEventListener('change', readFile, false);
+
+</script>
---
+++ split.js
@@ -0,0 +1,42 @@
+
+function readFile (evt) {
+	var files = evt.target.files;
+	var file = files[0];           
+	var reader = new FileReader();
+	reader.onload = function() {
+
+		var position = this.result.indexOf("\n");
+		string_result = this.result;
+		string_result = this.result.substring(position+1);
+		var csv_objects = $.csv.toObjects(string_result);
+
+		var zip = new JSZip();
+
+		for( var key in csv_objects ){
+			var row = csv_objects[key];
+			filename = row['channel'] + " " + row['date'] + " " + row['time'] + ".txt";
+			
+			if( row["Preview to longer segment? "] == '23' ){
+				content = row['Transcript Text:'] + "\n" + "yes";
+			}
+			else if( row["Preview to longer segment? "] == '24' ){
+				content = row['Transcript Text:'] + "\n" + "no";
+			}
+			
+			console.log( filename );
+			console.log( content );
+
+			zip.file(filename, content);
+
+		}
+		
+		zip.generateAsync({type:"blob"})
+		.then(function(content) {
+		    // see FileSaver.js
+		    saveAs(content, "split_text.zip");
+		});
+
+		
+	}
+	reader.readAsText(file)
+}
